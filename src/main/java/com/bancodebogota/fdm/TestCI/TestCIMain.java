7ede11ac83941072c5dde11bffeac0573bdc4502
ninojairo <nino.jairo@gmail.com>
2018-07-31 18:01:22 -0500

    First Commit

---
+++ src/main/java/com/bancodebogota/fdm/TestCI/TestCIMain.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bancodebogota.fdm.TestCI;
+
+/**
+ *
+ * @author jaironino
+ */
+
+
+public class TestCIMain {
+    
+    public int sum(int n1, int n2){
+        return n1+n2;
+    }
+    
+    public static void main(String[] args) {
+        System.err.println("TESTCI");
+    }
+}
